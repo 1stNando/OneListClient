849e81c60b7bf6ed44c50c9a8dda064838288163
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-01-30 20:44:25 -0500

    Set up synchronous connection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 namespace OneListClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
+
+            //the await keyword is the important part that lets us wait for the code. Which turns the 
+            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=fernando");
         }
     }
 }
