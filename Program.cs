4aa72ac3c3c5f86fb73b1eb27bed51c281c8a3d6
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-01-30 20:55:11 -0500

    Gets the response from the API as a simple string (not interpreted)

--- Program.cs
+++ Program.cs
@@ -9,8 +9,10 @@ namespace OneListClient
         {
             var client = new HttpClient();
 
-            //the await keyword is the important part that lets us wait for the code. Which turns the 
+            //the await keyword is important part that lets us wait for the code. Fetching data from a server and receiving back a string.
             var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=fernando");
+
+            Console.WriteLine(responseBodyAsString);
         }
     }
 }
