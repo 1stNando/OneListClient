6e9961ef62a44e88c8872a51d30691ea236cf465
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-01 12:06:19 -0500

    Custom DateTime and CompletedStatus

--- Item.cs
+++ Item.cs
@@ -10,8 +10,8 @@ namespace OneListClient
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
 
-        //read only property to improve on True/False status of completion. We customized the get to be able to do the logic we want it to do. 
-        public string CompletedStatus
+        //This is a custom property.read only property to improve on True/False status of completion. We customized the get to be able to do the logic we want it to do. 
+        public string CompletedStatus//we could make this a method instead and we can drop the get, and we call the method inside the {} 
         {
             get
             {
@@ -23,6 +23,9 @@ namespace OneListClient
                 {
                     return "Not Completed";
                 }
+
+                //Alternate great way: shorthand for if then else statement!
+                // return complete ? "Completed" : "Not Completed";
             }
 
         }
