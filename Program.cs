ed7cd791714edc2feb74e99d85e65a2075486987
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-01 11:26:07 -0500

    Updated response from string, to Stream the response from the API

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace OneListClient
             var client = new HttpClient();
 
             //the await keyword is important part that lets us wait for the code. Fetching data from a server and receiving back a string.
-            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=fernando");
+            var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=fernando");
 
             Console.WriteLine(responseBodyAsString);
         }
