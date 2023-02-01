a6c3f62bb157590a08d631daecf43d360888fc9b
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-01 16:51:02 -0500

    Ability to UPDATE one item

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ConsoleTables;
@@ -61,6 +62,76 @@ namespace OneListClient
             table.Write();
         }
 
+        static async Task AddOneItem(string token, Item newItem)
+        {
+            var client = new HttpClient();
+
+            // Generate a URL specifically referencing the endpoint for adding a todo item
+            var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
+
+            // Take the `newItem` and serialize it into JSON
+            var jsonBody = JsonSerializer.Serialize(newItem);
+
+            // We turn this into a StringContent object and indicate we are using JSON
+            // by ensuring there is a media type header of `application/json`
+            var jsonBodyAsContent = new StringContent(jsonBody);
+            jsonBodyAsContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            // Send the POST request to the URL and supply the JSON body
+            var response = await client.PostAsync(url, jsonBodyAsContent);
+
+            // Get the response as a stream.
+            var responseJson = await response.Content.ReadAsStreamAsync();
+
+            // Supply that *stream of data* to a Deserialize that will interpret it as a *SINGLE* `Item`
+            var item = await JsonSerializer.DeserializeAsync<Item>(responseJson);
+
+            // Make a table to output our new item.
+            var table = new ConsoleTable("ID", "Description", "Created At", "Updated At", "Completed");
+
+            // Add one row to our table
+            table.AddRow(item.Id, item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+
+            // Write the table
+            table.Write(Format.Minimal);
+        }
+
+        //implement UpdateItem method here:
+        static async Task UpdateOneItem(string token, int id, Item updatedItem)
+        {
+            var client = new HttpClient();
+
+            // Generate a URL specifically referencing the endpoint for getting a single
+            // todo item and provide the id we were supplied
+            var url = $"https://one-list-api.herokuapp.com/items/{id}?access_token={token}";
+
+            // Take the `newItem` and serialize it into JSON
+            var jsonBody = JsonSerializer.Serialize(updatedItem);
+
+            // We turn this into a StringContent object and indicate we are using JSON
+            // by ensuring there is a media type header of `application/json`
+            var jsonBodyAsContent = new StringContent(jsonBody);
+            jsonBodyAsContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            // Send the PUT request to the URL and supply the JSON body
+            var response = await client.PutAsync(url, jsonBodyAsContent);
+
+            // Get the response as a stream.
+            var responseJson = await response.Content.ReadAsStreamAsync();
+
+            // Supply that *stream of data* to a Deserialize that will interpret it as a *SINGLE* `Item`
+            var item = await JsonSerializer.DeserializeAsync<Item>(responseJson);
+
+            // Make a table to output our new item.
+            var table = new ConsoleTable("ID", "Description", "Created At", "Updated At", "Completed");
+
+            // Add one row to our table
+            table.AddRow(item.Id, item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+
+            // Write the table
+            table.Write(Format.Minimal);
+        }
+
         //Notice the "async Task" we have started to need ahead of the Main() in order for 
         //the system be become synchronous with await.
         static async Task Main(string[] args)
@@ -84,7 +155,7 @@ namespace OneListClient
             while (keepGoing)
             {
                 Console.Clear();
-                Console.Write("Get (A)ll to-do, (O)ne to-do, or (Q)uit: ");
+                Console.Write("Get (A)ll to-do, (O)ne to-do, (C)reate new item, or (Q)uit: ");
                 var choice = Console.ReadLine().ToUpper();
 
                 switch (choice)
@@ -109,6 +180,43 @@ namespace OneListClient
 
                         //token, id are the things it is taking in
                         await GetOneItem(token, id);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+
+                    case "C":
+                        Console.Write("Enter the description of your new todo: ");
+                        var text = Console.ReadLine();
+
+                        var newItem = new Item
+                        {
+                            Text = text
+                        };
+
+                        await AddOneItem(token, newItem);
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+
+                    case "U":
+                        Console.Write("Enter the ID of the item to update: ");
+                        var existingId = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter the new description: ");
+                        var newText = Console.ReadLine();
+
+                        Console.Write("Enter yes or no to indicate if the item is comoplete: ");
+                        var newComplete = Console.ReadLine().ToLower() == "yes";
+
+                        var updateItem = new Item
+                        {
+                            Text = newText,
+                            Complete = newComplete
+                        };
+
+                        await UpdateOneItem(token, existingId, updateItem);
+
                         Console.WriteLine("Press ENTER to continue");
                         Console.ReadLine();
                         break;
