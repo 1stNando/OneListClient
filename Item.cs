353f10299ca54a00b1c6901eb156ca661cae4b69
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-01 12:01:49 -0500

    Customized a { get; set; } to specific logic

--- Item.cs
+++ Item.cs
@@ -9,5 +9,22 @@ namespace OneListClient
         public bool complete { get; set; }
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
+
+        //read only property to improve on True/False status of completion. We customized the get to be able to do the logic we want it to do. 
+        public string CompletedStatus
+        {
+            get
+            {
+                if (complete == true)
+                {
+                    return "Completed";
+                }
+                else
+                {
+                    return "Not Completed";
+                }
+            }
+
+        }
     }
 }
