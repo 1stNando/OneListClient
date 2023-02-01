b905df63eebd8271bc1736160b8be93298e987ca
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-01 15:17:13 -0500

    Added ability to obtain the specific name of an existing List<> from the command line, dotnet run <name>

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ using ConsoleTables;
 namespace OneListClient
 {
     class Program
-    {
+    {   //Notice the "async Task" we have started to need ahead of the Main() in order for 
+        //the system be become synchronous with await.
         static async Task Main(string[] args)
         {
+            var token = args[0];
+
             var client = new HttpClient();
 
             //the await keyword is important part that lets us wait for the code. Fetching data from a server and receiving back a Stream!
-            var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=fernando");
+            var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items?access_token={token}");
 
             //                                  Describe the shape of the data (array in JSON => List, Object in JSON =>Item). This lives in the cloud.
             //                                               V          V
